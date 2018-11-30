d8161bd33e028963d785f728e4c4ed7229522341
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-11-30 13:26:18 +0100

    * Read/write TIFF IO

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@
 #include <coixx/image.h>
 #include <coixx/toolbox/toolbox_io.h>
 #include <iostream>
-#include <misaxx_ome/io/ome_read_write_tiff.h>
+#include <misaxx_ome/io/ome_tiff_io.h>
 #include <misaxx_ome/accessors/misa_ome_tiff.h>
 
 using namespace misaxx;
@@ -30,51 +30,70 @@ using namespace misaxx_ome;
 using namespace ome::files;
 using namespace ome::files::tiff;
 
-struct misa_ome_test_declaration : public misa_module_declaration {
-
-    misa_ome_tiff tiff;
-    misa_ome_tiff out_tiff;
-
-    void init_data() override {
-        tiff.suggest_import_location(filesystem, "/");
-        out_tiff.suggest_export_location(filesystem, "out_tiff", tiff.describe());
-    }
-};
-
-struct misa_ome_test : public misa_module<misa_ome_test_declaration> {
-    using misa_module<misa_ome_test_declaration>::misa_module;
-
-    void misa_init() override {
-        for(size_t i = 0; i < tiff.size(); ++i) {
-            std::cout << tiff.at(i).get_data_description() << std::endl;
-            auto read_access = tiff.at(i).access_readonly();
-            auto write_access = out_tiff.at(i).access_write();
-
-            write_access.set(read_access.get());
-        }
-    }
-};
-
-int main(int argc, const char** argv) {
-    misa_cli<misa_multiobject_root<misa_ome_test>> cli("test");
-    return cli.prepare_and_run(argc, argv);
-}
-
-//int main(int argc, const char** argv) {
+//struct misa_ome_test_declaration : public misa_module_declaration {
 //
-//    using namespace misaxx_ome;
-//    ome_read_write_tiff tiff("/home/rgerst/tmp/ome/input/object0/example_data.ome.tif");
-//    std::cout << tiff.get_metadata()->getImageCount() << std::endl;
-//    auto reader_access = tiff.get_reader();
+//    misa_ome_tiff tiff;
+//    misa_ome_tiff out_tiff;
 //
-//    for(size_t i = 0; i < tiff.get_metadata()->getImageCount(); ++i) {
-//        reader_access.value->setSeries(i);
-//        auto index = misa_ome_plane_location(i);
-//        auto end = misa_ome_plane_location::get_end(*reader_access.value, i);
-//        for(; index != end; index.increment_in_series(end)) {
-//            std::cout << index.t << " " << index.c << " " << index.z << std::endl;
+//    void init_data() override {
+//        tiff.suggest_import_location(filesystem, "/");
+//        out_tiff.suggest_export_location(filesystem, "out_tiff", tiff.describe());
+//    }
+//};
+//
+//struct misa_ome_test : public misa_module<misa_ome_test_declaration> {
+//    using misa_module<misa_ome_test_declaration>::misa_module;
+//
+//    void misa_init() override {
+//        for(size_t i = 0; i < tiff.size(); ++i) {
+//            std::cout << tiff.at(i).get_data_description() << std::endl;
+//            auto read_access = tiff.at(i).access_readonly();
+//            auto write_access = out_tiff.at(i).access_write();
+//
+//            write_access.set(read_access.get());
+//
+//            coixx::images::grayscale8u img(read_access.get());
+//            img << coixx::toolbox::show_and_wait("image");
 //        }
 //    }
+//};
 //
-//    return 0;
+//int main(int argc, const char** argv) {
+//    misa_cli<misa_multiobject_root<misa_ome_test>> cli("test");
+//    return cli.prepare_and_run(argc, argv);
 //}
+
+int main(int argc, const char** argv) {
+
+    using namespace misaxx_ome;
+//    ome_tiff_io tiff("/home/rgerst/tmp/ome/test/object0/example_data_Z0_C0_T0.ome.tif");
+    ome_tiff_io tiff("/home/rgerst/tmp/ome/test/split_test/ome_example_small.ome.tif");
+//    ome_tiff_io tiff("/home/rgerst/tmp/ome/test/split_test/ome_example_small_S0_Z0_C0_T0.ome.tif");
+    cv::Mat plane = tiff.read_plane(misa_ome_plane_location(0, 0, 0, 0));
+    tiff.write_plane(plane, misa_ome_plane_location(0, 0, 0, 0));
+
+//    for(size_t plane = 0; plane < tiff.get_num_planes(0); ++plane) {
+//        for(const auto &file : tiff.get_reader().value->getSeriesUsedFiles(false)) {
+//            std::cout << file << std::endl;
+//        }
+//    }
+
+//    tiff.get_writer();
+////    ome_tiff_io tiff("/home/rgerst/tmp/ome/test/object0/example_data_Z0_C0_T0.ome.tif");
+//    ome_tiff_io out_tiff("/home/rgerst/tmp/ome/test/split_test/" + cxxh::to_string(misa_ome_plane_location(0)) + ".ome.tif", tiff.get_metadata());
+//
+//    auto w = out_tiff.get_writer();
+//
+//    for(size_t i = 0; i < tiff.get_num_planes(0); ++i) {
+//        auto input_location = misa_ome_plane_location(0,  tiff.get_num_planes(0) - i - 1, 0, 0);
+//        std::cout << input_location << std::endl;
+//        cv::Mat input_image = tiff.read_plane(input_location);
+//
+//        w.value->changeOutputFile("/home/rgerst/tmp/ome/test/split_test/" + cxxh::to_string(input_location) + ".ome.tif");
+//        opencv_to_ome(input_image, *w.value, input_location);
+//    }
+//
+//    w.value->close();
+
+    return 0;
+}
--- misaxx_ome/io/ome_tiff_io.h
+++ misaxx_ome/io/ome_tiff_io.h
@@ -7,16 +7,40 @@
 
 #include <memory>
 #include <shared_mutex>
+#include <unordered_set>
 #include <ome/files/out/OMETIFFWriter.h>
 #include <ome/files/in/OMETIFFReader.h>
 #include <ome/xml/meta/Convert.h>
 #include <opencv2/opencv.hpp>
 #include <boost/filesystem.hpp>
 #include <cxxh/cache.h>
+#include <cxxh/string.h>
+#include <boost/regex.hpp>
 #include "ome_to_opencv.h"
 #include "opencv_to_ome.h"
+#include "ome_to_ome.h"
 
 namespace misaxx_ome {
+
+    /**
+    * Custom OMETIFFWriter that removes the sequential write check
+    */
+    struct non_sequential_ome_tiff_writer : public ome::files::out::OMETIFFWriter {
+        using OMETIFFWriter::OMETIFFWriter;
+
+        void setPlane(ome::files::dimension_size_type plane) const override {
+            const ome::files::dimension_size_type currentPlane = getPlane();
+
+            this->plane = plane;
+
+            if (currentPlane != plane)
+            {
+                nextIFD();
+                setupIFD();
+            }
+        }
+    };
+
     /**
      * Allows thread-safe read and write access to an OME TIFF
      * This wrapper will automatically switch between an OME TIFF reader and an OME TIFF writer depending on what functionality
@@ -28,7 +52,7 @@ namespace misaxx_ome {
     public:
 
         using locked_reader_type = cxxh::locked<std::shared_ptr<ome::files::in::OMETIFFReader>, std::shared_lock<std::shared_mutex>>;
-        using locked_writer_type = cxxh::locked<std::shared_ptr<ome::files::out::OMETIFFWriter>, std::shared_lock<std::shared_mutex>>;
+        using locked_writer_type = cxxh::locked<std::shared_ptr<ome::files::out::OMETIFFWriter>, std::unique_lock<std::shared_mutex>>;
 
         ome_tiff_io() = default;
 
@@ -37,6 +61,7 @@ namespace misaxx_ome {
          * @param t_path
          */
         explicit ome_tiff_io(boost::filesystem::path t_path) : m_path(std::move(t_path)) {
+            m_buffer_path = m_path.parent_path() / (get_base_filename().string() + "_wbuffer.ome.tif");
             if (!boost::filesystem::is_regular_file(m_path)) {
                 throw std::runtime_error("Cannot read from non-existing file " + m_path.string());
             }
@@ -51,6 +76,7 @@ namespace misaxx_ome {
         explicit ome_tiff_io(boost::filesystem::path t_path,
                                      std::shared_ptr<ome::xml::meta::OMEXMLMetadata> t_metadata) : m_path(
                 std::move(t_path)), m_metadata(std::move(t_metadata)) {
+            m_buffer_path = m_path.parent_path() / (get_base_filename().string() + "_wbuffer.ome.tif");
             // We can load metadata from the file if it exists
             if (boost::filesystem::is_regular_file(m_path)) {
                 m_metadata.reset();
@@ -71,6 +97,8 @@ namespace misaxx_ome {
                 throw std::runtime_error("Only series 0 is currently supported!");
             auto writer = get_writer();
 //            writer.value->setSeries(index.series); // TODO: Mutex
+            const auto writer_path = m_path.parent_path() / (get_base_filename().string() + "_" + cxxh::to_string(index) + ".ome.tif");
+            writer.value->changeOutputFile(writer_path);
             opencv_to_ome(image, *writer.value, index);
         }
 
@@ -82,6 +110,10 @@ namespace misaxx_ome {
             return ome_to_opencv(*reader.value, index);
         }
 
+        /**
+         * Thread-safe access to the metadata
+         * @return
+         */
         std::shared_ptr<ome::xml::meta::OMEXMLMetadata> get_metadata() const {
             if(static_cast<bool>(m_metadata)) {
                 return m_metadata;
@@ -117,31 +149,10 @@ namespace misaxx_ome {
                 }
                 open_reader();
                 wlock.unlock();
-                return get_reader();
-            }
-        }
 
-        /**
-         * Thread-safe access to the managed writer
-         * @return
-         */
-        locked_writer_type get_writer() const {
-            std::shared_lock<std::shared_mutex> lock(m_mutex, std::defer_lock);
-            lock.lock();
-            if(static_cast<bool>(m_writer)) {
-                return locked_writer_type(m_writer, std::move(lock));
-            }
-            else {
-                lock.unlock();
-                std::unique_lock<std::shared_mutex> wlock(m_mutex, std::defer_lock);
-                wlock.lock();
-                if(static_cast<bool>(m_reader)) {
-                    m_reader->close();
-                    m_reader.reset();
-                }
-                open_writer();
-                wlock.unlock();
-                return get_writer();
+                // Return the locked reader
+                lock.lock();
+                return locked_reader_type(m_reader, std::move(lock));
             }
         }
 
@@ -152,12 +163,10 @@ namespace misaxx_ome {
             std::unique_lock<std::shared_mutex> wlock(m_mutex, std::defer_lock);
             wlock.lock();
             if(static_cast<bool>(m_reader)) {
-                m_reader->close();
-                m_reader.reset();
+                close_reader();
             }
             if(static_cast<bool>(m_writer)) {
-                m_writer->close();
-                m_writer.reset();
+                close_writer();
             }
         }
 
@@ -189,67 +198,137 @@ namespace misaxx_ome {
             return get_size_c(series) * get_size_t(series) * get_size_z(series);
         }
 
+    protected:
+
+        /**
+        * Thread-safe access to the managed writer
+        * The writer is unique-locked (sequential access!)
+        * @return
+        */
+        locked_writer_type get_writer() const {
+            std::unique_lock<std::shared_mutex> lock(m_mutex, std::defer_lock);
+            lock.lock();
+            if(static_cast<bool>(m_writer)) {
+                return locked_writer_type(m_writer, std::move(lock));
+            }
+            else {
+
+                // If the current TIFF file exists, set it to a reader->writer connection
+                // This will preserve the TIFF data that is already written
+                if(boost::filesystem::is_regular_file(m_path)) {
+                    if(!static_cast<bool>(m_reader)) {
+                        open_reader();
+                    }
+                }
+
+                open_writer();
+                return locked_writer_type(m_writer, std::move(lock));
+            }
+        }
+
+        /**
+         * Returns the filename of the path without .ome.tif extension
+         * @return
+         */
+        boost::filesystem::path get_base_filename() const {
+            auto base_name = m_path.filename();
+            for(int i = 0; i < 2 && base_name.has_extension(); ++i) {
+                base_name.replace_extension();
+            }
+            return base_name;
+        }
+
     private:
-        boost::filesystem::path m_path;
+        /**
+         * Path of the TIFF that is read / written
+         */
+        mutable boost::filesystem::path m_path;
+        /**
+         * Buffer TIFF file if a reader is switched to a writer
+         */
+        mutable boost::filesystem::path m_buffer_path;
         mutable std::shared_ptr<ome::files::out::OMETIFFWriter> m_writer;
         mutable std::shared_ptr<ome::files::in::OMETIFFReader> m_reader;
         mutable std::shared_ptr<ome::xml::meta::OMEXMLMetadata> m_metadata;
         mutable std::shared_mutex m_mutex;
 
-        /**
-         * Opens the reader
-         * NOT THREAD SAFE!
-         */
         void open_reader() const {
             m_reader = std::make_shared<ome::files::in::OMETIFFReader>();
             m_reader->setMetadataFiltered(false);
             m_reader->setGroupFiles(true);
             m_reader->setId(m_path);
+
             if(!static_cast<bool>(m_metadata)) {
                m_metadata = ome::files::createOMEXMLMetadata(*m_reader);
             }
         }
 
-        /**
-         * Opens the writer
-         * NOT THREAD SAFE!
-         */
+        void close_reader() const {
+            m_reader->close();
+            m_reader.reset();
+        }
+
         void open_writer() const {
             if(!static_cast<bool>(m_metadata)) {
                 throw std::runtime_error("Cannot write to OME TIFF " + m_path.string() + " without necessary metadata!");
             }
             auto metadata = std::static_pointer_cast<ome::xml::meta::MetadataRetrieve>(m_metadata);
-            m_writer = std::make_shared<ome::files::out::OMETIFFWriter>();
+            m_writer = std::make_shared<non_sequential_ome_tiff_writer>(); // Initialize a non-sequential TIFF writer
             m_writer->setMetadataRetrieve(metadata);
             m_writer->setInterleaved(false);
             m_writer->setWriteSequentially(false);
+
+            // If the existing write target already exists, use the buffer path instead
+            if(boost::filesystem::is_regular_file(m_path)) {
+                std::swap(m_path, m_buffer_path);
+            }
+
             m_writer->setId(m_path);
+
+            if(static_cast<bool>(m_reader)) {
+                // We need to copy the data from reader to writer (to preserve it)
+                write_all_tiffs();
+                close_reader();
+            }
+        }
+
+        void close_writer() const {
+            if(boost::filesystem::is_regular_file(m_buffer_path)) {
+                // Switch to the write buffer
+                std::swap(m_path, m_buffer_path);
+            }
         }
 
         /**
-         * Gets a property that is only accessible via the reader / writer instance
-         * @tparam Function
-         * @param t_function
-         * @return
+         * Copies all TIFF data from the reader to the writer
          */
-        template<typename T, typename Function> T get_reader_writer_property(const Function &t_function) const {
-            std::shared_lock<std::shared_mutex> lock(m_mutex, std::defer_lock);
-            lock.lock();
-            if(static_cast<bool>(m_reader)) {
-                return t_function(m_reader);
+        void write_all_tiffs() const {
+            std::cout << "[MISA++ OME] Switching " << m_path << " from read to write mode ... " << std::endl;
+
+            for(size_t series = 0; series < get_num_series(); ++series) {
+                m_reader->setSeries(series);
+                m_writer->setSeries(series);
+
+                const auto size_Z = m_reader->getSizeZ();
+                const auto size_C = m_reader->getSizeC();
+                const auto size_T = m_reader->getSizeT();
+
+                for(size_t z = 0; z < size_Z; ++z) {
+                    for(size_t c = 0; c < size_C; ++c) {
+                        for (size_t t = 0; t < size_T; ++t) {
+                            const auto location_name = cxxh::to_string(misa_ome_plane_location(series, z, c, t));
+                            std::cout << "[MISA++ OME] Switching " << m_path << " from read to write mode ... " << location_name << std::endl;
+
+                            // Change the writer path
+                            const auto writer_path = m_path.parent_path() / (get_base_filename().string() + "_" + location_name + ".ome.tif");
+                            m_writer->changeOutputFile(writer_path);
+
+                            // Copy via variant pixel buffer
+                            ome_to_ome(*m_reader, misa_ome_plane_location(series, z, c, t), *m_writer, misa_ome_plane_location(series, z, c, t));
+                        }
+                    }
+                }
             }
-            else if(static_cast<bool>(m_writer)) {
-                return t_function(m_writer);
-            }
-            else if(boost::filesystem::exists(m_path)) {
-                lock.unlock();
-                open_reader();
-            }
-            else {
-                lock.unlock();
-                open_writer();
-            }
-            return get_reader_writer_property<T, Function>(t_function);
         }
     };
 }
