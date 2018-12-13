53c595df2504ccc95bce38020fb7c01091c3ba91
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2018-12-13 15:38:32 +0100

    * fix

--- misaxx_ome/attachments/misa_ome_voxel_size.h
+++ misaxx_ome/attachments/misa_ome_voxel_size.h
@@ -14,7 +14,10 @@ namespace misaxx_ome {
      * Higher-order wrapper around a vector of 3 length-unit values, modelling the size of a voxel
      */
     struct misa_ome_voxel_size : public misaxx::misa_serializeable {
+
         using matrix_type = misaxx::misa_vector<double, misaxx_ome::misa_ome_unit_length <1>, 3>;
+        using unit_type = typename matrix_type::unit_type;
+        using ome_unit_type = typename unit_type::ome_unit_type;
 
         /**
          * Matrix that contains
@@ -36,17 +39,29 @@ namespace misaxx_ome {
          * @param t_meta
          * @param t_series
          */
-        explicit misa_ome_voxel_size(const ome::xml::meta::OMEXMLMetadata &t_meta, size_t t_series = 0) {
+        explicit misa_ome_voxel_size(const ome::xml::meta::OMEXMLMetadata &t_meta, size_t t_series = 0, std::optional<unit_type> t_unit = std::nullopt) {
+
             auto x = t_meta.getPixelsPhysicalSizeX(t_series);
             auto y = t_meta.getPixelsPhysicalSizeY(t_series);
             auto z = t_meta.getPixelsPhysicalSizeZ(t_series);
 
+            unit_type u;
+            if(static_cast<bool>(t_unit)) {
+                u = *t_unit;
+            }
+            else {
+                u = x.get_unit();
+            }
+
             // Convert all to the same unit
+            if(u != x.getUnit()) {
+                x = ome::xml::model::primitives::convert(x, static_cast<ome_unit_type>(u));
+            }
             if(x.getUnit() != y.getUnit()) {
-                x = ome::xml::model::primitives::convert(y, x.getUnit());
+                x = ome::xml::model::primitives::convert(y, static_cast<ome_unit_type>(u));
             }
             if(x.getUnit() != z.getUnit()) {
-                z = ome::xml::model::primitives::convert(z, x.getUnit());
+                z = ome::xml::model::primitives::convert(z, static_cast<ome_unit_type>(u));
             }
 
             // Pass to the matrix
@@ -72,6 +87,5 @@ namespace misaxx_ome {
         auto get_size_z() const {
             return values.get(2, 0);
         }
-
     };
 }
