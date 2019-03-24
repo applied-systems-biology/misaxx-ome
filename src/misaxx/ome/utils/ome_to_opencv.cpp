801b53cc664181c13d02b69f63c406ee42266b45
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-03-24 15:26:47 +0100

    * fix

--- src/misaxx/ome/utils/ome_to_opencv.cpp
+++ src/misaxx/ome/utils/ome_to_opencv.cpp
@@ -1,6 +1,44 @@
 #include "ome_to_opencv.h"
+#include <ome/files/VariantPixelBuffer.h>
 
 using namespace misaxx::ome;
+
+namespace {
+    /**
+    * Converts a OME variant pixel buffer into a cv::Mat
+    * @tparam RawType
+    * @param ome_buffer
+    * @param size_x
+    * @param size_y
+    * @param channels
+    * @param opencv_type
+    * @return
+    */
+    template<typename RawType> inline cv::Mat ome_to_opencv_detail(const ::ome::files::VariantPixelBuffer &ome_buffer, int size_x, int size_y, int channels, int opencv_type) {
+        cv::Mat result(size_y, size_x, opencv_type, cv::Scalar::all(0));
+
+        if(!result.isContinuous())
+            throw std::runtime_error("cv::Mat must be continuous!");
+
+        const auto &src_array = ome_buffer.array<RawType>();
+
+        ::ome::files::PixelBufferBase::indices_type idx;
+        std::fill(idx.begin(), idx.end(), 0);
+
+        for(int y = 0; y < result.rows; ++y) {
+            auto *ptr = result.ptr<RawType>(y);
+            for(int x = 0; x < result.cols; ++x) {
+                idx[::ome::files::DIM_SPATIAL_X] = x;
+                idx[::ome::files::DIM_SPATIAL_Y] = y;
+                for(int c = 0; c < channels; ++c) {
+                    idx[::ome::files::DIM_SUBCHANNEL] = c;
+                    ptr[x * channels + c] = src_array(idx);
+                }
+            }
+        }
+        return result;
+    }
+}
 
 cv::Mat misaxx::ome::ome_to_opencv(const ::ome::files::FormatReader &ome_reader, const misa_ome_plane_description &index) {
 
